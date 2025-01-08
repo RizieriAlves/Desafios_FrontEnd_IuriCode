9e0e7c5d44a9cb6c4a5166eec854a8244da597f3
RizieriAlves <viniciusrizieri@hotmail.com>
2025-01-08 12:21:37 -0300

    Feat - Criação do Primeiro Projeto + cabeçalho

---
+++ CodeLab/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <p>
+          Code<span>Lab</span>
+        </p>
+        <input type="text" placeholder="Pesquisar no blog" />
+      </header>
+    </>
+  );
+}
+
+export default App;
