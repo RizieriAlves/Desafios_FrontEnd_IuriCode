0fe6fbfff347e2183534b8b87ad2b3755d7ea613
RizieriAlves <viniciusrizieri@hotmail.com>
2025-01-14 08:44:52 -0300

    feat - Criação de documento e criação de Header. style - Estilização de Header

---
+++ TechBlog/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import "../scss/header.scss";
+import headerimage from "../images/headerimage.png";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <div className="header-text">
+          <span className="header-text-title">
+            Encontre os <span>melhores artigos </span>
+            de programação em um só lugar
+          </span>
+          <span className="header-text-subtitle">
+            Explore o topo da programação em um só lugar! Seu destino único para
+            dicas e tendências atuais.
+          </span>
+          <button className="header-text-button">BUSCAR ARTIGOS</button>
+        </div>
+        <img src={headerimage} alt="Pessoa feliz no computador" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
