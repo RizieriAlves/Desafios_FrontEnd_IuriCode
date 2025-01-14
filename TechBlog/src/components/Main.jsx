6e0a2bb5291755bdaf2bd6b54d28f678369315ba
RizieriAlves <viniciusrizieri@hotmail.com>
2025-01-14 08:59:53 -0300

    feat - Criação geral do componente Main style - Estilização inicial

---
+++ TechBlog/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import "../scss/Main.scss";
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="container-content">
+        <span className="container-content-title">Artigos recomendados</span>
+        <div className="container-content-articles"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
